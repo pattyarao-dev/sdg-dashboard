652475d8632e6644881b6665b17f5a8143ffa02c
W-Choudai <daniellemariempua5@gmail.com>
2025-02-26 01:54:54 +0800

    updated api

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 from fastapi import FastAPI
-from api import etl  
+from fastapi.routing import APIRoute
+from api import etl
+from api import compute
 
 app = FastAPI()
 
 # Include ETL routes
 app.include_router(etl.router)
+app.include_router(compute.router)
+
+for route in app.routes:
+    print(route.path)
 
 @app.get("/")
 async def root():
